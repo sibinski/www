997cf468c62ac142ed33e67ec5f1c2d7309d329b
Jevgeni Shibinski <158762167+sibinski@users.noreply.github.com>
2025-12-25 20:50:07 +0200

    Add inside_the_function.py with a sample function

---
+++ inside_the_function.py
@@ -0,0 +1,10 @@
+"""Inside the function."""
+
+
+# Write your code here
+def inside_the_function():
+    """Tagasta sõnumit."""
+    return ('I´m inside the function\n')
+
+
+print(inside_the_function())
